2f85fecfce33ec6d76a8106f5303e5bd826b226a
jorhedev <hijorhe@gmail.com>
2024-09-11 19:55:10 -0600

    update id of database

--- src/routes/edmBack.js
+++ src/routes/edmBack.js
@@ -4,8 +4,7 @@ const controllers = require("../controllers/edmNewsControllers");
 const edmNews = Router();
 
 edmNews.get("/", controllers.getEdmNews);
-edmNews.post("/createNews" , controllers.postCreateNews);
-
-
+edmNews.post("/createNews", controllers.postCreateNews);
+edmNews.get("/:id", controllers.getEdmNewsById);
 
 module.exports = edmNews;
